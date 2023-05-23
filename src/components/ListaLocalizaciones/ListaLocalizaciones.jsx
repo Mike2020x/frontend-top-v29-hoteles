4a0419d46d11d0fc07927920210283c5258c80fc
Juan Xavier <100261001+juanxavier357@users.noreply.github.com>
2023-05-23 14:19:37 -0500

    Componentes Estrellas, Hotel, Lista de Hoteles, Oferta, Lista de Ofertas, Localizacion, Lista de Localizaciones y Titulos (#6)

---
+++ src/components/ListaLocalizaciones/ListaLocalizaciones.jsx
@@ -0,0 +1,49 @@
+import Localizacion from '../Localizacion/Localizacion';
+import './ListaLocalizaciones.scss';
+
+function ListaLocalizaciones() {
+  const localizaciones = [
+    {
+      imageLocation: './south-africa.jpg',
+      title: 'South Africa',
+      description: 'Starting From $5000',
+    },
+    {
+      imageLocation: './india.jpg',
+      title: 'India',
+      description: 'Starting From $5000',
+    },
+    {
+      imageLocation: './australia.jpg',
+      title: 'Australia',
+      description: 'Starting From $5000',
+    },
+    {
+      imageLocation: './europe.jpg',
+      title: 'Europe',
+      description: 'Starting From $5000',
+    },
+    {
+      imageLocation: './america.jpg',
+      title: 'America',
+      description: 'Starting From $5000',
+    },
+    {
+      imageLocation: './japan.jpg',
+      title: 'Japan',
+      description: 'Starting From $5000',
+    },
+  ];
+
+  return (
+    <>
+      <div className='lista__localizaciones'>
+        {localizaciones.map((localizacion, index) => (
+          <Localizacion key={index} {...localizacion} />
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default ListaLocalizaciones;
