2e510e0cd323a957c9dc3e248b292c1b9c5f3f2b
Michael <62442141+Mike2020x@users.noreply.github.com>
2023-05-23 12:26:20 -0500

    Feature/dashboard (#5)
    * feat: add footer component
    
    * feat: add dashboard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/home'
+// import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import Home from './pages/home'
 import NavBar from './components/navbar/NavBar'
+import Home from "./pages/Home"
+
 import Footer from './components/footer/Footer'
 import './App.css'
 
@@ -8,13 +10,14 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
-        <NavBar />
-        <Routes >
-          <Route path='/' element={<Home />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      {/* <BrowserRouter> */}
+      <NavBar />
+      {/* <Routes > */}
+      {/* <Route path='/' element={} /> */}
+      {/* </Routes> */}
+      <Home />
+      <Footer />
+      {/* // </BrowserRouter > */}
     </>
   )
 }
