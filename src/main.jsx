713adb535b3c165c3c8b01130431becef6cc203c
Michael <62442141+Mike2020x@users.noreply.github.com>
2023-05-29 18:11:45 -0500

    feat: add Login component (#13)

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from "react-router-dom";
 import Router from "./router";
-
+import "./main.scss"
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={Router} />
