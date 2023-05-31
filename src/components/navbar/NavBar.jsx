c5d97fc81a68cc27f3d6a61c97ce2a59ad0023dc
Juan Xavier <100261001+juanxavier357@users.noreply.github.com>
2023-05-30 22:19:28 -0500

    Fix/menu-mobile (#16)
    * Fix: NavBar fixed with structure and styles scss
    
    * fix: add menu mobile sidebar y also other fixes

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,33 +1,39 @@
-// import { NavLink } from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faUser, faGear, faCircleCheck } from '@fortawesome/free-solid-svg-icons'
-import "./index.scss"
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faUser, faGear, faCircleCheck, faAngleLeft } from '@fortawesome/free-solid-svg-icons';
+import "./index.scss";
 
 export default function NavBar() {
+  const [showSidebar, setShowSidebar] = useState(false);
+
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar);
+  };
+
   return (
-    <nav className="navigation__bar">
+    <nav className={`navigation__bar ${showSidebar ? 'show-sidebar' : ''}`}>
       <div className="navigation__bar--logo">
         <FontAwesomeIcon icon={faCircleCheck} size="2xl" />
         <h1>RICA</h1>
       </div>
       <ul className="navigation__bar--items">
-          <li><a href="/" className="pestana">HOME</a></li>
-          <li><a href="/pages" className="pestana">PAGES</a></li>
-        </ul>
+        <li><a href="/" >HOME</a></li>
+        <li><a href="/pages" >PAGES</a></li>
+      </ul>
       <div className="navigation__bar--content">
-        <button className='nav-bar__buttons--item'>
+        <button onClick={toggleSidebar} className="nav-bar__buttons--item">
           <FontAwesomeIcon icon={faBars} />
         </button>
         <div className="configuration">
           <div className="navigation__bar--items">
-            <select className='money'>
+            <select className="money">
               <option value="USD">USD</option>
               <option value="PEN">PEN</option>
               <option value="COP">COP</option>
             </select>
           </div>
           <div className="navigation__bar--items">
-            <select className='language'>
+            <select className="language">
               <option value="ENG">ENG</option>
               <option value="ESP">SPA</option>
               <option value="FRA">FRA</option>
@@ -36,13 +42,16 @@ export default function NavBar() {
           <button>
             <FontAwesomeIcon icon={faUser} />
           </button>
-          <button id="faGear" className='nav-bar__buttons--item'>
+          <button id="faGear" className="nav-bar__buttons--item">
             <FontAwesomeIcon icon={faGear} />
           </button>
         </div>
       </div>
+      <div className={`sidebar ${showSidebar ? 'show' : ''}`}>
+        <button onClick={toggleSidebar} className="back">BACK <FontAwesomeIcon icon={faAngleLeft} rotation={180} /></button>
+        <button><a href="/" className="home">HOME</a></button>
+        <button><a href="/pages" className="pages">PAGES</a></button>
+      </div>
     </nav>
-
-  )
+  );
 }
-
