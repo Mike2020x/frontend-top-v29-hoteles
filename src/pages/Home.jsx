897fa979a9742ea50d8500cd5d5f4bb773776bd1
Mike2020x <62442141+Mike2020x@users.noreply.github.com>
2023-06-01 12:53:52 -0500

    feat: add booking success page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,17 @@
 import Titulos from "../components/Titulos/Titulos";
 import ListaOfertas from "../components/ListaOfertas/ListaOfertas";
 import ListaLocalizaciones from "../components/ListaLocalizaciones/ListaLocalizaciones";
+import BookingSuccess from "./BookingSuccess";
 
 export default function Home() {
   return (
     <div>
-      <Titulos />
+      {/* <Titulos />
       <ListaOfertas />
-      <ListaLocalizaciones />
+      <ListaLocalizaciones /> */}
+      <BookingSuccess />
     </div>
   )
 }
+
+
