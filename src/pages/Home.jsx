71126fbd9acf8ee8e25ad80be8e5b6b04482bfb6
juanxavier357 <cabellosalirrosas@gmail.com>
2023-06-02 14:35:15 -0500

    Fix: component Hotel List in progress

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,19 @@
-import Titulos from "../components/Titulos/Titulos";
-import ListaOfertas from "../components/ListaOfertas/ListaOfertas";
-import ListaLocalizaciones from "../components/ListaLocalizaciones/ListaLocalizaciones";
-import BookingSuccess from "./BookingSuccess";
-import Blog from "../components/Blog/Blog";
+//import Titulos from "../components/Titulos/Titulos";
+//import ListaOfertas from "../components/ListaOfertas/ListaOfertas";
+//import ListaLocalizaciones from "../components/ListaLocalizaciones/ListaLocalizaciones";
+//import Blog from "../components/Blog/Blog";
+//import Footer from "../components/footer/Footer";
+//import BookingSuccess from "./BookingSuccess";
+
 
 export default function Home() {
   return (
     <div>
       {/* <Titulos />
       <ListaOfertas />
-      <ListaLocalizaciones /> */}
-      <BookingSuccess />
+      <ListaLocalizaciones />
+      <Blog />
+      <Footer />*/}
     </div>
   )
 }
---
+++ src/pages/HotelList/HotelList.jsx
@@ -0,0 +1,14 @@
+import Titulos from '../../components/Titulos/Titulos'
+import ListaHoteles from '../../components/ListaHoteles/ListaHoteles'
+import Footer from '../../components/footer/Footer'
+import 'index.scss'
+
+export default function HotelList() {
+  return (
+    <div>
+      <Titulos />
+      <ListaHoteles />
+      <Footer />
+    </div>
+  )
+}
