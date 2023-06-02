207ed4740223a4e129fbd173e4ba2906e7f28783
Michael <62442141+Mike2020x@users.noreply.github.com>
2023-06-02 14:12:15 -0500

    feat: add booking success page (#22)
    * feat: add booking success page
    
    * feat: add test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,18 @@
+import Titulos from "../components/Titulos/Titulos";
+import ListaOfertas from "../components/ListaOfertas/ListaOfertas";
+import ListaLocalizaciones from "../components/ListaLocalizaciones/ListaLocalizaciones";
+import BookingSuccess from "./BookingSuccess";
 import Blog from "../components/Blog/Blog";
-// import Titulos from "../components/Titulos/Titulos";
-// import ListaOfertas from "../components/ListaOfertas/ListaOfertas";
-// import ListaLocalizaciones from "../components/ListaLocalizaciones/ListaLocalizaciones";
-//import UserDashboard from "./UserDashboard";
 
 export default function Home() {
   return (
     <div>
       {/* <Titulos />
       <ListaOfertas />
-      <ListaLocalizaciones />
-      <UserDashboard />*/}
-      <Blog />
+      <ListaLocalizaciones /> */}
+      <BookingSuccess />
     </div>
   )
 }
+
+
