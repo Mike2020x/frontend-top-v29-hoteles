0052515864c078ecb888b2e4cafc547efd645773
Michael <62442141+Mike2020x@users.noreply.github.com>
2023-06-03 18:00:55 -0500

    Feature/checkout page (#26)
    * feat: add booking success page
    
    * feat: add test
    
    * feat: add Booking summary
    
    * style: add booking summary styles
    
    * feat: add traveller info
    
    * feat: add traveler info
    
    * feat: add responsive design
    
    * feat: add checkout model page
    
    ---------
    
    Co-authored-by: Juan Xavier <100261001+juanxavier357@users.noreply.github.com>

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,15 @@
 import Titulos from "../components/Titulos/Titulos";
 import ListaOfertas from "../components/ListaOfertas/ListaOfertas";
 import ListaLocalizaciones from "../components/ListaLocalizaciones/ListaLocalizaciones";
-import BookingSummaryPage from "./bookinSummaryPage/BookingSummaryPage";
+import Blog from "../components/Blog/Blog";
 
 export default function Home() {
   return (
     <div>
-      {/* <Titulos />
+      <Titulos />
       <ListaOfertas />
-      <ListaLocalizaciones /> */}
-      <BookingSummaryPage />
+      <ListaLocalizaciones />
+      <Blog />
     </div>
   )
 }
-
-
