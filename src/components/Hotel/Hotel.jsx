fa2e63461979f19644b31d7a86b38e8550f06430
juanxavier357 <cabellosalirrosas@gmail.com>
2023-06-04 11:01:35 -0500

    Fix: Component Room, Page Hotel Single and other improvements in components

--- src/components/Hotel/Hotel.jsx
+++ src/components/Hotel/Hotel.jsx
@@ -1,43 +1,53 @@
 import Estrellas from '../Estrellas/Estrellas'
 import PropTypes from 'prop-types'
 import './Hotel.scss'
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-function Hotel({ imageHotel, imageLogo, title, location, description, pastprice, actualprice }) {
+function Hotel({ imageHotel, title, location, description, pastprice, actualprice }) {
 
-    return (
-        <>
-            <div className='contenedor__cardhotel'>
-                <img src={imageHotel} className="image" alt={imageHotel} width="360px" />
-                <div className='contenedor__espacio'>
-                    <div className='contenedor__locacion'>
-                        <h1>{title}</h1>
-                        <img src={imageLogo} className="logo__location" alt={imageLogo} width="14px" height="14" />
-                        <h4>{location}</h4>
-                    </div>
-                    <p>{description}</p>
-                    <div className='contenedor__stars'>
-                        <Estrellas></Estrellas>
-                    </div >
-                    <div className="priceService">
-                        <h3><del>${pastprice}</del></h3>
-                        <h2>${actualprice}</h2>
-                        <button>Swimming</button>
-                        <button>Parking</button>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className='contenedor__cardhotel'>
+        <img src={imageHotel} className="image" alt={imageHotel} width="360px" />
+        <div className='contenedor__espacio'>
+          <div className='contenedor__location'>
+            <h1>{title}</h1>
+            <div>
+              <h4><FontAwesomeIcon icon={faLocationDot} /> {location}</h4>
             </div>
-        </>
-    )
+          </div>
+          <p>{description}</p>
+          <div className='contenedor__stars'>
+            <Estrellas/>
+            <div className='revisiones'>
+              <h4>26412 review</h4>
+            </div>
+          </div >
+          <div className="priceService">
+            <div className='priceHotel'>
+              <h3><del>${pastprice}</del></h3>
+              <h2>${actualprice}</h2>
+            </div>
+            <div className='priceHotel'>
+              <button>Swimming</button>
+              <button>Parking</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 Hotel.propTypes = {
-    imageHotel: PropTypes.string,
-    imageLogo: PropTypes.string,
-    title: PropTypes.string,
-    location: PropTypes.string,
-    description: PropTypes.string,
-    pastprice: PropTypes.string,
-    actualprice: PropTypes.string,
+  imageHotel: PropTypes.string,
+  imageLogo: PropTypes.string,
+  title: PropTypes.string,
+  location: PropTypes.string,
+  description: PropTypes.string,
+  pastprice: PropTypes.string,
+  actualprice: PropTypes.string,
 }
 
 export default Hotel
