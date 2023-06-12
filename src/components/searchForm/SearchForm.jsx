44fc7604ad1ecb2d7725f45a498f32d3a4f59763
Xavier Cabello <100261001+juanxavier357@users.noreply.github.com>
2023-06-12 11:26:31 -0500

    Feature/title-ask (#59)
    * Cambios momentaneos en los nombres en componentes css con el objetivo de encontrar el error en el componente ListaOfertas
    
    * fix: improvement component Title, add searchForm folder and another adjustment

--- src/components/searchForm/SearchForm.jsx
+++ src/components/searchForm/SearchForm.jsx
@@ -1,33 +1,139 @@
-import "./index.scss"
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './index.scss';
+
 export default function SearchForm() {
+  const navigate = useNavigate();
+
+  const [hotel, setHotel] = useState('');
+  const [checkIn, setCheckIn] = useState(getCurrentDate());
+  const [checkOut, setCheckOut] = useState(getNextDay(getCurrentDate()));
+  const [guests, setGuests] = useState(1);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+
+    if (name === 'hotel') {
+      setHotel(value);
+    } else if (name === 'checkIn') {
+      setCheckIn(value);
+      setCheckOut(getNextDay(value));
+    } else if (name === 'checkOut') {
+      setCheckOut(value);
+    } else if (name === 'guests') {
+      setGuests(value);
+    }
+  };
+
+  const handleSearch = () => {
+    if (!hotel) {
+      alert('Por favor, complete el campo del hotel.');
+      return;
+    }
+
+    const searchParams = new URLSearchParams();
+    searchParams.set('hotel', hotel);
+    searchParams.set('checkIn', checkIn);
+    searchParams.set('checkOut', checkOut);
+    searchParams.set('guests', guests);
+
+    navigate(`/hotel-list?${searchParams.toString()}`);
+  };
+
   return (
-    <div className="search-form-container">
-      <h4 className='search-form__title'>BOOK ROOMS,HOMES & APTS</h4>
-      <div className="search-form">
-        <div className="search-form__item">
-          <label htmlFor="hotel">Hotel</label>
-          <input type="text" id="hotel" placeholder="Where are you going" className="search-for--input" />
+    <div className='content__titles'>
+      <h1>BOOK ROOMS, HOMES & APTS</h1>
+      <div className='content__titles--search'>
+        <div className='content__titles--box'>
+          <div className='content__titles--mobileBox'>
+            <div className='content__titles--border'>
+              <h4>HOTEL</h4>
+              <input
+                type="text"
+                className='location'
+                name="hotel"
+                value={hotel}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="border__right1"></div>
+            <div className='content__titles--border'>
+              <h4>CHECK IN</h4>
+              <input
+                type="date"
+                id="checkIn"
+                className="search-for--input"
+                name="checkIn"
+                value={checkIn}
+                min={getCurrentDate()} // Establecer la fecha mínima como la fecha actual
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="border__right2"></div>
+          </div>
+          <div className='content__titles--mobileBox'>
+            <div className='content__titles--border'>
+              <h4>CHECK OUT</h4>
+              <input
+                type="date"
+                id="checkOut"
+                className="search-for--input"
+                name="checkOut"
+                value={checkOut}
+                min={checkIn} // Establecer la fecha mínima como el valor de check-in
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="border__right1"></div>
+            <div className='content__titles--border'>
+              <h4>GUESTS</h4>
+              <input
+                className="choose_guest"
+                placeholder={guests}
+                type="number"
+                min="1"
+                max="5"
+                name="guests"
+                value={guests}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="border__right2"></div>
+          </div>
+          <div className='content__titles--box search'>
+            {hotel ? (
+              <Link to={`/hotel-list?hotel=${hotel}&checkIn=${checkIn}&checkOut=${checkOut}&guests=${guests}`}>
+                <button>SEARCH</button>
+              </Link>
+            ) : (
+              <button onClick={handleSearch}>SEARCH</button>
+            )}
+          </div>
         </div>
-        <div className="search-form__item">
-          <label htmlFor="">Check In</label>
-          <input type="date" id="checkOut" className="search-for--input" />
-        </div>
-        <div className="search-form__item">
-          <label htmlFor="">Check Out</label>
-          <input type="date" id="checkIn" className="search-for--input" />
-        </div>
-        <div className="search-form__item">
-          <label htmlFor="guests">Guests</label>
-          <select type="select" id="guests" >
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-          </select >
-        </div>
-        <input type="submit" value="Search" />
       </div>
-    </div >
-  )
+    </div>
+  );
+}
+
+function getCurrentDate() {
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  let month = currentDate.getMonth() + 1;
+  let day = currentDate.getDate();
+
+  if (month < 10) {
+    month = `0${month}`;
+  }
+
+  if (day < 10) {
+    day = `0${day}`;
+  }
+
+  return `${year}-${month}-${day}`;
+}
+
+function getNextDay(dateString) {
+  const currentDate = new Date(dateString);
+  const nextDay = new Date(currentDate.getTime() + 24 * 60 * 60 * 1000);
+  return nextDay.toISOString().split('T')[0];
 }
