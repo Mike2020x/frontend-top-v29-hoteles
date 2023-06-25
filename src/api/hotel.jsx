36e381f402e47623af77fb964e053b5673dfe638
Xavier Cabello <100261001+juanxavier357@users.noreply.github.com>
2023-06-25 13:04:47 -0500

    Feature/api hotel (#61)
    * Cambios momentaneos en los nombres en componentes css con el objetivo de encontrar el error en el componente ListaOfertas
    
    * feat: :sparkles: add api/hotel
    
    show hotels with api from backend

--- src/api/hotel.jsx
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'postgres://db_user:h0n0VLi5id9tvEqwYBVacsfyDnUkE4P4@dpg-chv5oo0rddl9p8irasn0-a.ohio-postgres.render.com/dbhotels',
-});
-
-export default api;
