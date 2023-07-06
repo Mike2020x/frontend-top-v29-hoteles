9ca125caa18eebeeb2536020df8524aee97ecb4e
juanxavier357 <cabellosalirrosas@gmail.com>
2023-07-06 16:05:09 -0500

    feat: some changes

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -36,4 +36,5 @@ export const HotelProvider = ({ children }) => {
   );
 };
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useHotel = () => useContext(HotelContext);
